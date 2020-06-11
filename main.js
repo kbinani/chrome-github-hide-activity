980a206adc4574cb8ab25e42870a0c8f810d4a9b
kbinani <kbinani.bt@gmail.com>
2020-06-12 02:10:34 +0900

    Hide more activities

--- main.js
+++ main.js
@@ -16,59 +16,75 @@ $(function() {
     });
   };
 
-  $("body").on({
-    DOMNodeInserted: async () => {
-      var github_username = "";
-      try {
-        github_username = await promiseGetCompanyGitHubUsername();
-      } catch (e) {
-        console.log(e);
-        return;
-      }
+  async function onTick(click) {
+    var github_username = "";
+    try {
+      github_username = await promiseGetCompanyGitHubUsername();
+    } catch (e) {
+      console.log(e);
+      return;
+    }
 
-      $("div.js-recent-activity-container").each(function() {
-        const container = $(this);
-        const ul = container.find("ul");
-        ul.children().each(function() {
-          const li = $(this);
-          const a = li.find("a");
-          if (a) {
-            const href = a.attr("href");
-            if (href.indexOf("/" + github_username + "/") == 0) {
-              li.remove();
-            }
-          }
-        });
-        if (ul.children().length == 0) {
-          const show_more_button = ul.next();
-          ul.remove();
-          if (show_more_button) {
-            const display = show_more_button.css("display");
-            if (typeof(display) != "undefined") {
-              show_more_button.click();
-            }
-          }
-          const h4 = container.prev();
-          container.remove();
-          h4.remove();
-        }
-      });
-
-      $("div.push,div.watch_started,div.follow,div.repo").each(function() {
-        const push = $(this);
-        var contains_link = false;
-        push.find("a").each(function() {
-          const a = $(this);
+    $("div.js-recent-activity-container").each(function() {
+      const container = $(this);
+      const ul = container.find("ul");
+      ul.children().each(function() {
+        const li = $(this);
+        const a = li.find("a");
+        if (a) {
           const href = a.attr("href");
           if (href.indexOf("/" + github_username + "/") == 0) {
-            contains_link = true;
-            return false;
+            li.remove();
           }
-        });
-        if (contains_link) {
-          push.remove();
         }
       });
-    }
+      if (ul.children().length == 0) {
+        const show_more_button = ul.next();
+        ul.remove();
+        if (show_more_button) {
+          const display = show_more_button.css("display");
+          if (typeof(display) != "undefined" && click) {
+            show_more_button.click();
+          }
+        }
+        const h4 = container.prev();
+        container.remove();
+        h4.remove();
+      }
+    });
+
+    $("div.push,div.watch_started,div.follow,div.repo,div.release,div.issues_labeled").each(function() {
+      const push = $(this);
+      var contains_link = false;
+      push.find("a").each(function() {
+        const a = $(this);
+        const href = a.attr("href");
+        if (href.indexOf("/" + github_username + "/") == 0) {
+          contains_link = true;
+          return false;
+        }
+      });
+      if (contains_link) {
+        push.remove();
+      }
+    });
+  };
+
+  $("body").on({
+    DOMNodeInserted: async () => {
+      onTick(true);
+    },
   });
+
+  $(window).on('load', () => {
+    onTick(false);
+  });
+
+  window.onpopstate = () => {
+    onTick(false);
+  };
+
+  const tid = setInterval(() => {
+     onTick(false);
+  }, 100);
 });
